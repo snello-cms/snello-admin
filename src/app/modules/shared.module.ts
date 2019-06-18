76329a0c91d89942dfa28cd47fe3f55b676b0385
Andrea Montanari <andrea.montanari@ict-group.it>
2019-06-18 20:31:25 +0200

    slimming

--- src/app/modules/shared.module.ts
+++ src/app/modules/shared.module.ts
@@ -5,25 +5,23 @@ import {RouterModule} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ToastModule} from 'primeng/toast';
 
-import {
-  AutoCompleteModule,
-  BlockUIModule,
-  CalendarModule,
-  CheckboxModule,
-  ChipsModule,
-  ConfirmDialogModule,
-  DropdownModule,
-  FieldsetModule,
-  FileUploadModule,
-  InputSwitchModule,
-  InputTextareaModule,
-  MessageModule,
-  MessagesModule,
-  PanelModule,
-  ProgressBarModule,
-  RadioButtonModule,
-  TabViewModule
-} from 'primeng/primeng';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {CheckboxModule} from 'primeng/checkbox';
+import {CalendarModule} from 'primeng/calendar';
+import {ChipsModule} from 'primeng/chips';
+import {InputSwitchModule} from 'primeng/inputswitch';
+import {DropdownModule} from 'primeng/dropdown';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import {PanelModule} from 'primeng/panel';
+import {FileUploadModule} from 'primeng/fileupload';
+import {ProgressBarModule} from 'primeng/progressbar';
+import {TabViewModule} from 'primeng/tabview';
+import {FieldsetModule} from 'primeng/fieldset';
+import {BlockUIModule} from 'primeng/blockui';
+import {RadioButtonModule} from 'primeng/radiobutton';
 import {TableModule} from 'primeng/table';
 import {MultiSelectModule} from 'primeng/multiselect';
 
