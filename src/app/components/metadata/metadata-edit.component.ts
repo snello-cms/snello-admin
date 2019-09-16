7532963e18cbfb9d80cf366f9e268c620a8cb157
fiorenzo pizza <fiorenzino@gmail.com>
2019-09-17 01:01:01 +0200

    miglioriamo

--- src/app/components/metadata/metadata-edit.component.ts
+++ src/app/components/metadata/metadata-edit.component.ts
@@ -21,6 +21,7 @@ export class MetadataEditComponent extends AbstractEditComponent<Metadata>
 
     public newtable = true;
     public advanced = false;
+    public msgs: any;
 
     constructor(
         router: Router,
--- src/app/generic.components/tinymce/tinymce.component.ts
+++ src/app/generic.components/tinymce/tinymce.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 import {FieldDefinition} from '../../model/field-definition';
+import {ASSET_PATH} from '../../constants/constants';
+import {ConfigurationService} from '../../service/configuration.service';
 
 @Component({
     selector: 'app-tinymce',
@@ -8,7 +10,7 @@ import {FieldDefinition} from '../../model/field-definition';
         <div class="form-group clearfix row" [formGroup]="group">
             <label class="col-sm-3">{{ field.name }}</label>
             <div class="col-sm-9">
-                <editor [formControlName]="field.name" [init]="{ base_url: './tinymce',suffix: '.min', entity_encoding: 'numeric', plugins: 'table hr link image',  menubar: 'false', height: 300, toolbar: 'image | table | hr | link | removeformat | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent'}"></editor>
+                <editor [formControlName]="field.name" [init]="{ base_url: asset_path+'tinymce',suffix: '.min', entity_encoding: 'numeric', plugins: 'table hr link image',  menubar: 'false', height: 300, toolbar: 'image | table | hr | link | removeformat | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent'}"></editor>
                 <ng-container *ngFor="let validation of field.validations">
                 </ng-container>
             </div>
@@ -19,8 +21,14 @@ import {FieldDefinition} from '../../model/field-definition';
 export class TinymceComponent implements OnInit {
     field: FieldDefinition;
     group: FormGroup;
+    asset_path: string;
 
-    constructor() {
+    constructor(configurationService: ConfigurationService) {
+        configurationService.getValue(ASSET_PATH).subscribe(
+            path => {
+                console.log(path);
+                this.asset_path = path;
+            });
     }
 
     ngOnInit() {
