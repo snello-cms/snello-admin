7b30727182b24a12048719b9b020c256ae2368c0
Andrea Montanari <andrea.montanari@ict-group.it>
2019-10-27 23:12:07 +0100

    correct call

--- src/app/generic.components/join/join.component.ts
+++ src/app/generic.components/join/join.component.ts
@@ -9,7 +9,7 @@ import { tap, take } from 'rxjs/operators';
 @Component({
     selector: 'app-join',
     template: `
-        <div *ngIf="join | async" class="form-group clearfix" [formGroup]="group">
+        <div *ngIf="join$ | async as value" class="form-group clearfix" [formGroup]="group">
             <div class="row">
                 <label class="col-sm-3">
                     {{ field.name }}
@@ -30,7 +30,7 @@ export class JoinComponent implements OnInit {
     field: FieldDefinition;
     group: FormGroup;
 
-    join: Observable<any>;
+    join$: Observable<any>;
     options: any[] = [];
     labelField: string;
     labelMap: Map<string, any> = new Map();
@@ -54,8 +54,8 @@ export class JoinComponent implements OnInit {
         this.uuid = this.activatedRoute.snapshot.params['uuid'];
         this.name = this.activatedRoute.snapshot.params['name'];
 
-        this.join =
-            this.apiService.fetchJoinList(this.name, this.uuid, this.field.join_table_name)
+        this.join$ =
+            this.apiService.fetchObject(this.field.join_table_name, this.field.value)
             .pipe(
                 tap(join => this.group.get(this.field.name).setValue(join)),
                 take(1)
