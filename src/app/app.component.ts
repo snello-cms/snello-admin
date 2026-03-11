8971cd52abb2da3b12ceb7553f49c57f29394ca4
fiorenzo pizza <fiorenzino@gmail.com>
2026-03-11 15:59:44 +0100

    removal of &iss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {NavigationEnd, Router} from '@angular/router';
 import {filter} from 'rxjs/internal/operators';
 import {take} from 'rxjs/operators';
 
